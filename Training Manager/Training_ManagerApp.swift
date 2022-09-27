b82be8dc41e0f90ffc40e9e23c4fc66b57f72c81
k-isaka <isk.rugby@gmail.com>
2022-09-28 00:08:37 +0900

    first commit

---
+++ Training Manager/Training_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Training_ManagerApp.swift
+//  Training Manager
+//
+//  Created by 井坂航 on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct Training_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

eb1e715620a4a3869e2d03c4c9740ea66ac845f8
k-isaka <isk.rugby@gmail.com>
2024-09-23 12:18:12 +0900

    feat: タイマーの開始、停止ボタンを円の外に配置

--- Training Manager/View/TodaySession/TimerView.swift
+++ Training Manager/View/TodaySession/TimerView.swift
@@ -11,31 +11,33 @@ struct TimerView: View {
     @ObservedObject var viewModel:TimerViewModel
     
     var body: some View {
-        ZStack{
-            if(viewModel.isAlarmActive){
-                VStack{
-                    Button(action: {viewModel.stopAlarm()}, label: {
-                        Text("アラーム停止").bold().padding(10)
-                            .foregroundColor(.white)
-                        .background(.orange,in: RoundedRectangle(cornerRadius: 12))})
+        HStack(alignment: .bottom){
+            ZStack{
+                if(viewModel.isAlarmActive){
+                    VStack{
+                        Button(action: {viewModel.stopAlarm()}, label: {
+                            Text("アラーム停止").bold().padding(10)
+                                .foregroundColor(.white)
+                            .background(.orange,in: RoundedRectangle(cornerRadius: 12))})
+                    }
+                }else{
+                    VStack(alignment: .center) {
+                        Text(viewModel.timeString)
+                            .font(.headline)
+                            .padding(5)
+                    }.onReceive(viewModel.$timeString) { newValue in}
+                    CircleProgressBarView(progress: $viewModel.progress).frame(maxWidth: 80)
                 }
-            }else{
-                VStack(alignment: .center) {
-                    Text(viewModel.timeString)
-                        .font(.headline)
-                        .padding(5)
-                    Button(action: {
-                        if viewModel.timer == nil {
-                            viewModel.start() // タイマー開始
-                        } else {
-                            viewModel.stop() // タイマー停止
-                        }
-                    }) {
-                        Text(viewModel.timer == nil ? "開始" : "停止").fontWeight(.bold)
-                    }.buttonStyle(.borderless)
-                }.onReceive(viewModel.$timeString) { newValue in}
-                CircleProgressBarView(progress: $viewModel.progress).frame(maxWidth: 80)
             }
+            Button(action: {
+                if viewModel.timer == nil {
+                    viewModel.start() // タイマー開始
+                } else {
+                    viewModel.stop() // タイマー停止
+                }
+            }) {
+                Image(systemName: viewModel.timer == nil ? "play.circle.fill" : "pause.circle.fill").foregroundStyle(viewModel.timer == nil ? Color.blue : Color.red)
+            }.buttonStyle(.borderless)
         }.padding(5)
     }
 }

ee2dfafb0f9efd610a3d60e3e23ebee666e46872
k-isaka <isk.rugby@gmail.com>
2024-10-01 09:34:01 +0900

    feat: 練習メニュー削除機能を追加

--- Training Manager/View/TrainingMenuHistory/TrainingMenuHistory.swift
+++ Training Manager/View/TrainingMenuHistory/TrainingMenuHistory.swift
@@ -9,25 +9,61 @@ import SwiftUI
 
 struct TrainingMenuHistory: View {
     @ObservedObject var contentViewModel: ContentViewModel
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var isEditMode: Bool = false
+    @State private var menuToDelete: TrainingMenu?
+    @State private var isShowDeleteAlart: Bool = false
     
     var body: some View {
-        VStack{
-            Text("メニュー履歴").font(.title)
-            List(contentViewModel.trainingMenuList.sorted(by: { $0.orderIndex < $1.orderIndex })){menu in
-                VStack(alignment: .leading) {
-                    HStack(alignment: .center){
-                        Text(menu.name).font(.headline)
-                        HStack{
-                            Image(systemName: "stopwatch")
-                            Text(formatDuration(duration: menu.duration ?? 0))
-                        }.foregroundStyle(.white).fontWeight(.bold).padding(5).background(.green).cornerRadius(30)
+        NavigationStack {
+            VStack{
+                List(contentViewModel.trainingMenuList.sorted(by: { $0.orderIndex < $1.orderIndex })){ menu in
+                    HStack{
+                        if isEditMode {
+                            HStack{
+                                Button(action: {
+                                    menuToDelete = menu // 削除対象のメニューを設定
+                                    isShowDeleteAlart.toggle()
+                                }, label:{
+                                    Image(systemName: "minus.circle.fill").foregroundStyle(.red)
+                                }).buttonStyle(.borderless).background(.clear)
+                                    .alert("メニューの削除", isPresented: $isShowDeleteAlart, actions: {
+                                        Button("削除", role: .destructive) {
+                                            print(menu)
+                                            if let menuToDelete = menuToDelete {
+                                                deleteMenu(menu: menuToDelete) // 削除対象のメニューを削除
+                                            }
+                                        }
+                                        Button("キャンセル", role: .cancel) {}
+                                    })
+                            }
+                        }
+                        VStack(alignment: .leading) {
+                            HStack(alignment: .center){
+                                Text(menu.name).font(.headline)
+                                HStack{
+                                    Image(systemName: "stopwatch")
+                                    Text(formatDuration(duration: menu.duration ?? 0))
+                                }.foregroundStyle(.white).fontWeight(.bold).padding(5).background(.green).cornerRadius(30)
+                            }
+                            Text(menu.goal).font(.subheadline).underline()
+                            ForEach(menu.focusPoints, id: \.self){ point in
+                                Text(point).font(.caption)
+                            }
+                            if(menu.menuDescription != "" || menu.menuDescription != nil){
+                                Text(menu.menuDescription ?? "").font(.caption).foregroundStyle(.gray)
+                            }
+                        }
                     }
-                    Text(menu.goal).font(.subheadline).underline()
-                    ForEach(menu.focusPoints, id: \.self){point in
-                        Text(point).font(.caption)
-                    }
-                    if(menu.menuDescription != "" || menu.menuDescription != nil){
-                        Text(menu.menuDescription ?? "").font(.caption).foregroundStyle(.gray)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        isEditMode.toggle()
+                    } label: {
+                        Text(isEditMode ? "完了" : "編集")
                     }
                 }
             }
@@ -35,10 +71,24 @@ struct TrainingMenuHistory: View {
     }
     
     // 時間をmm:ss形式で表示
-    func formatDuration(duration: TimeInterval) -> String {
+    private func formatDuration(duration: TimeInterval) -> String {
         let minutes = Int(duration) / 60
         let seconds = Int(duration) % 60
         return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    // メニューを削除する処理
+    private func deleteMenu(menu: TrainingMenu) {
+        contentViewModel.trainingMenuList.removeAll(where: {$0.id == menu.id})
+        // データベースから削除
+        modelContext.delete(menu)
+        // データベースに保存
+        do {
+            try modelContext.save() // 変更を保存
+        } catch {
+            print("Failed to save context: \(error)")
+        }
+        self.isEditMode = false
     }
 }
 
